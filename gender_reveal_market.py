62376b01796b2801a8aab586d0a4c35fe2b9428f
jtanuri <71709280+jtanuri@users.noreply.github.com>
2025-06-17 13:51:03 +0700

    Update gender_reveal_market.py

--- gender_reveal_market.py
+++ gender_reveal_market.py
@@ -35,7 +35,7 @@ st.markdown(
         When the actual gender is revealed, all the money from those who bet incorrectly is pooled and shared among the winners in proportion to how much they bet.
         The fewer amount bet correctly, the larger each winner's payout will be — so if the 'Girl' pool is much smaller and 'Girl' is correct,
         the payout multiplier will be much higher. The multiplier is calculated at the end of the event. So if you place a bet on girl when theres 15jt in the boy pool
-        and 1 jt in the girl pool, you are not guaranteed a large multiplier if the pools reaches equilibrium
+        and 1 jt in the girl pool, you are not guaranteed a large multiplier if the pools reach equilibrium
         <br><br>
         <em>Note:</em> 50% of each winner’s profit is automatically donated to Nathan’s foundation and/or YCAB.
         <br><br>
